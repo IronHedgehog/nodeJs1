032b975a54dbad57ff4a8123f9efe126a509b160
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-05-25 17:14:13 +0300

    contacts.js

--- contacts.js
+++ contacts.js
@@ -1,27 +1,72 @@
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const contactPath = path.resolve("./db/contacts.json");
-
-console.log(contactPath);
 
 function listContacts() {
   fs.readFile(contactPath, "utf-8", (err, data) => {
     if (err) {
-      console.log("err", err);
+      console.error("err", err);
     }
-    console.log("data", data);
+    console.table("data", data);
   });
 }
 
-// function getContactById(contactId) {
-// ...твой код
-// }
+function getContactById(contactId) {
+  fs.readFile(contactPath, "utf-8", (err, data) => {
+    if (err) {
+      console.error("err", err);
+    }
+    console.log(
+      JSON.parse(data).find(({ id }) => String(id) === String(contactId))
+    );
+  });
+}
 
-// function removeContact(contactId) {
-// ...твой код
-// }
+function removeContact(contactId) {
+  fs.readFile(contactPath, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+    }
 
-// function addContact(name, email, phone) {
-// ...твой код
-// }
+    const updateContacts = JSON.parse(data).filter(
+      ({ id }) => String(id) !== String(contactId)
+    );
+
+    fs.writeFile(contactPath, JSON.stringify(updateContacts), (err) => {
+      if (err) {
+        console.error("ERROR", err);
+      }
+    });
+  });
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactPath, "utf-8", (err, data) => {
+    if (err) {
+      console.error("err", err);
+    }
+    let contactAll = JSON.parse(data);
+    contactAll.push({
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    });
+    fs.writeFile(contactPath, JSON.stringify(contactAll), (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+}
+
+const methods = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
+
+module.exports = methods;
